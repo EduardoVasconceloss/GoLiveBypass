31a45261f080e670f5efab12f79cc50fd304637a
Eduardo Vasconcelos <eduardo.vasconcelos@cubos.io>
2026-08-19 01:22:32 -0300

    instalador: bump do GLB_URL para o commit com o fix do fechamento apos winget

--- installer/GoLiveBypass-Installer.bat
+++ installer/GoLiveBypass-Installer.bat
@@ -19,7 +19,7 @@ rem TEM QUE ser um commit onde o .ps1 baixado JA TENHA o RepoRaw dele proprio fi
 rem "main"): apontar para um commit anterior a essa correcao baixaria um .ps1 que ainda busca
 rem o codigo do plugin em main, reabrindo o mesmo buraco um passo adiante. Atualizar isto para
 rem o HEAD atual faz parte de cortar uma release nova do instalador.
-set "GLB_URL=https://raw.githubusercontent.com/EduardoVasconceloss/GoLiveBypass/9c1c5cc/installer/GoLiveBypass-Installer.ps1"
+set "GLB_URL=https://raw.githubusercontent.com/EduardoVasconceloss/GoLiveBypass/5d2c3a5/installer/GoLiveBypass-Installer.ps1"
 
 rem Baixa sempre para um arquivo temporario, nunca por cima do que ja existe: uma correcao de
 rem bug publicada no fork so vale de algo se quem ja tentou instalar antes (e tem um .ps1
